49d187605189cbde509fa5ac23976f3c3237f307
HamnaBaig <hamnabaig19@gmail.com>
2020-07-26 23:12:50 -0400

    code refactoring

--- src/test/java/com/hamna/tdd/tddtask1/TddTask1Test.java
+++ src/test/java/com/hamna/tdd/tddtask1/TddTask1Test.java
@@ -1,11 +1,7 @@
 package com.hamna.tdd.tddtask1;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
-
-
-import junit.framework.Assert;
+import org.junit.Assert;
 
 public class TddTask1Test {
 	@Test
@@ -13,22 +9,14 @@ public class TddTask1Test {
 		TddTask1 sArray = new TddTask1();
 		String input = "1 2 3";
 		int sum =sArray.sumArray(input);
-		Assert.assertEquals(6, sum);
-		//@Test
-		//public void verifySumArray() {
-//			TddTask1 sArray = new TddTask1();
-//			int sum =sArray.sumArray("1 2 4");
-//			Assert.assertEquals(7, sum);
-		//	
-
-		
+		Assert.assertEquals(6, sum);	
 	}
+
 	@Test
 	public void verifyValidValue() {
 		TddTask1 vValue = new TddTask1();
-	String[] array = {"100", "200000"};
+	    String[] array = {"100", "200000"};
 		boolean validValue =  vValue.getValidValue(array);
 		Assert.assertEquals(false, validValue);
-
 	}
 }
